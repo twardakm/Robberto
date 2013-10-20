197433392811910399d8c3a52b83f9d0b1f6e873
twardakm <twardakm@gmail.com>
2013-10-20 13:49:02 +0200

    1.03v Wyłączony JTAG

--- main.c
+++ main.c
@@ -7,16 +7,7 @@ int main(void)
 {
 
 	MCUCSR |=(1<<JTD);
-
-	DDRA = 0xff;
-	DDRB = 0xff;
-	DDRC = 0xff;
-	DDRD = 0xff;
-
-	PORTA = 0xff;
-	PORTB = 0xff;
-	PORTC = 0xff;
-	PORTD = 0xff;
+	MCUCSR |=(1<<JTD);
 
     return 0;
 }
